88ccc42b667eb973653f803b5c23440791795781
akazupin <akazupinsan@gmail.com>
2017-09-20 20:06:12 +0900

    a

--- src/phone/Phone_Test.java
+++ src/phone/Phone_Test.java
@@ -4,26 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import service.BaseService;
+
 public class Phone_Test {
 
 	@Test
 	public void testPhone_default() {
 		Phone phone = new Phone("090-1111-1111");
-
 		assertEquals("090-1111-1111" , phone.getPhoneNumber());
-		assertEquals(1000 , phone.getMonthlyBasicFee());
-		assertEquals(0 , phone.getMonthlyCallFee());
+
+		BaseService testBaseService = new BaseService();
 	}
-
-	@Test
-	public void testPhone_set() {
-
-		Phone phone = new Phone("090-1111-1111");
-
-		assertEquals("090-1111-1111" , phone.getPhoneNumber());
-		assertEquals(1000 , phone.getMonthlyBasicFee());
-		assertEquals(0 , phone.getMonthlyCallFee());
-	}
-
-
 }
